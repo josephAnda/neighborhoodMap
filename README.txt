370db38fda19e4e0c3f290dd3871ea2d448e00e7
Joseph Anda <orenmurasaki@gmail.com>
2015-08-15 22:05:25 -0700

    fixed marker bug

---
+++ README.txt
@@ -0,0 +1,15 @@
+Neighborhood Mapper V.1.0.0 by Joseph Anda
+
+Using the Mapper:
+
+1)  Search a type of venue by entering it into the search bar (e.g., 'coffee shops') and clicking the 'search' button.
+2)  View filter, and explore results in the list
+3)  Search new venues by entering another query
+
+About Neighborhood Mapper:
+
+This project is intended to showcase a few major front-end development-related skills:
+
+1)  Using organizational libraries (in this case, knockout and jQuery)
+2)  Applying the MVVM paradigm for separation of concerns
+3)  Utilizing AJAX reqeusts to relay third-party info to the user
--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,7 @@
 //  TODO:
+//  [  ]  FIX ViewModel property definition bug
 //  [  ]  Ensure appropriate separation of concerns via MVVM paradigm
-//  [  ]  Restructure code to initialize markers, fix the locale in one position
+//  [  ]  Encapsulate defaults in a model object
 //  [  ]  Add map markers
 //  [  ]  Add searching and filtering functionality (what does this mean???)
 //  [  ]  Implement a list view of identified locations
@@ -14,50 +15,119 @@
 
 	//  Loads initial map
 	var $mapCanvas = $("#map-canvas")[0];  //  Reference to map-canvas div
-	var lat = 37.56; //  Lattitude for San Mateo
-	var lng = -122.32;
+	var lat = 37.56; //  Lattitude for Downtown San Mateo
+	var lng = -122.32;  //  Longitude for Downtown San Mateo
 
-	function location(name, lat, lng, phone, url) {
+
+	//  Prototype object to be populated with properties of JSON response from Foursquare request
+	function Place( name, lat, lng, phone, url ) {
 		this.name = name;
 		this.lat = lat;
 		this.lng = lng;
 		this.phone = phone;
 		this.url = url;
 	}
-	function initializeMap() {
+
+	//  Default points of interest, fallbacks if AJAX request fails
+	var Places = [{
+		name: "name",
+		lattitude:  0,
+		longitude:  0,
+		contact:  '000-000-0000',
+		url: ""
+	},
+	{
+		name: "name",
+		lattitude:  0,
+		longitude:  0,
+		contact:  '000-000-0000',
+		url: ""
+
+	}];
+		
+	
+	function initializeMap( results ) {
 		var mapOptions = {
-  				center: new google.maps.LatLng(lat, lng),
-  				zoom: 15
+					center: new google.maps.LatLng(lat, lng),
+					zoom: 15
 			};
-		var map = new google.maps.Map( $mapCanvas, mapOptions);
+		var map = new google.maps.Map( $mapCanvas, mapOptions );
+		if ( results ) {
+			//console.log(neighborhoodViewModel());  //  <--- Tells me that it can't set 'place' property of 'undefined' . . . .
+			console.log(results);
+			//neighborhoodViewModel().addMarkers(places, map);
+		};
 		
 	}
+	
 
 	//  Holds ViewModel functionality
 	function neighborhoodViewModel() {
-		var self = this;
-		//this.address = ko.observable();  //  Bound to the address that the user inputs
-		this.place = ko.observable();  //  Bound to the place that the user searches for 
-		this.markers = ko.observableArray();  //  Tracks the markers 
+		var self = this;  //  Allows 'this' to be used within nested scopes
+		this.place = ko.observable();  //  Bound to the place that the user searches for later use in functions 
+		this.results = ko.observableArray();  //  Tracks the places in an observable 'results' array 
+		this.markers = ko.observableArray();
+		//  Creates new Google map object
 
+		//  AJAX request to Foursquare bound to user clicking 'Search' button
 		this.getPlace = function() { 
 			$.getJSON("https://api.foursquare.com/v2/venues/search?client_id=DFMQLSBHUH2LQAQ3DQYSNSAR3TYCNHQJ3DEIHVKSMK0KBGPJ&client_secret=3J5U50Y3HOGLN3DJDHROLSZB4FBHEZCNW1P3VWHANK4KRNYO&v=20130815&ll=" + 
 			lat + "," + lng + "&query=" + self.place(), function( data ) {
-				console.log( data );
+				
+				var places = data.response.venues;  //  Pulls the array of search results from the JSON response 
+				console.log( places );  //  Test . . . . 
+				
+				//  Populates 'results' array with venue information stored in 'Place' prototype 
+				self.results.removeAll();  //  Clears previous results
+				$.each( places, function( index, item ) {
+					var place = new Place(item.name, item.location.lat, item.location.lng, item.contact.phone, item.url);
+					self.results.push(place);
+					
+				});
+				  //  Test . . . 
+				
+			console.log(self.results());
+			self.initializeMap(self.results());
 			});
+			
+			
 			return false   //  DON'T refresh the page!!!
 		};
-		function addMarker(location, map) {
+
+		this.addMarkers = function( places, map ) {
   		// Add the marker at the clicked location, and add the next-available label
   		// from the array of alphabetical characters.
-  			var marker = new google.maps.Marker({
-    			position: location,
-    			label: markerLabels[labelIndex++ % markerLabels.length],
-    			map: map
-  			});
+  		// The function below is courtesty of a Google Developers example
+		// Each marker is labeled with a single alphabetical character.
+			var markerLabels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			var labelIndex = 0;
+			self.markers.removeAll(); //  Clears previous markers
+			$.each( places, function ( index, item) {
+				var marker = new google.maps.Marker({
+    				position: {lat: item.lat, lng: item.lng},
+    				label: markerLabels[labelIndex++ % markerLabels.length],
+    				map: map
+  				});
   			self.markers.push(marker);
+
+
+			})
+  			
   			console.log(self.markers());
 		};
+
+		this.initializeMap = function( results ) {
+			var mapOptions = {
+						center: new google.maps.LatLng(lat, lng),
+						zoom: 15
+				};
+			var map = new google.maps.Map( $mapCanvas, mapOptions );
+			if ( results ) {
+				console.log(results);
+				self.addMarkers(results, map);
+			};
+		
+		}
 		/*
         this.getMap = function() {
         	if (self.markers != 0) { self.markers.removeAll() };  //  Resets marker list when the page loads
@@ -91,8 +161,11 @@
     };
 
     //  Open up the map at the default location and zoom
-    initializeMap();
+    new neighborhoodViewModel().initializeMap();
 
     //  Bind the ViewModel to the html
     ko.applyBindings( new neighborhoodViewModel() );
+    
+    
+    
 })();
